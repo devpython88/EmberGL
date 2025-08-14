982e7e8a21e20fc092b5ee37018f81bf918f97f3
UniDev69 <devpython88@gmail.com>
2025-08-14 21:17:18 +0530

    Delete build directory

--- build/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/Users/Sadik/Documents/ember/build
-/usr/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- build/run.bat
+++
@@ -1,6 +0,0 @@
-cd build
-ninja -j4
-if errorlevel 0 (
-    .\test.exe
-)
-cd..
